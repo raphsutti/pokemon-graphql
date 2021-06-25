d057c9d2c7ee47733c2705788ccb3b096c8319cc
rsutti <neoster9@gmail.com>
2021-06-26 09:40:24 +1000

    Add sprites, moves, types

--- src/pokemon.ts
+++ src/pokemon.ts
@@ -5,13 +5,55 @@ const typeDefs = gql`
   type Query {
     getPokemon(id: Int!): Pokemon
   }
+
   type Pokemon {
     weight: Int
+    sprites: Sprites
     species: Species
+    moves: [Move]
+    types: [Type]
   }
+
+  type Sprites {
+    front_default: String
+    front_shiny: String
+  }
+
   type Species {
     name: String
     url: String
+  }
+  
+  type Move {
+    move: MoveDetail
+  }
+
+  type MoveDetail {
+    name: String
+    url: String
+  }
+  
+  type Type {
+    slot: Int
+    type: TypeDetail
+  } 
+
+  type TypeDetail {
+    name: String
+    damage: Damage
+  }
+
+  type Damage {
+    damage_relations: DamageRelations
+  }
+
+  type DamageRelations {
+    double_damage_from: [Element]
+    half_damage_from: [Element]
+  }
+
+  type Element {
+    name: String
   }
 `;
 
@@ -22,6 +64,12 @@ const resolvers = {
       return response.json();
     },
   },
+  TypeDetail: {
+    damage: async(parent) => {
+      const response = await fetch(parent.url)
+      return response.json();
+    }
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
